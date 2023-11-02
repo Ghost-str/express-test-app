d5e1b1e0187628532de5026daca9b5b625dfefdc
ghost-str <Ghost-str@users.noreply.github.com>
2023-11-02 17:13:00 +0700

    added readme

--- src/bootstrap/database.js
+++ src/bootstrap/database.js
@@ -2,10 +2,10 @@ import pg from "pg";
 
 export default async function databaseConnection() {
   const pool = new pg.Pool({
-    host: process.env.DBHOST || "localhost",
-    port: process.env.DBPORT || 5432,
-    user: process.env.DBUSER || "postgres",
-    password: process.env.DBPASSWORD || "password",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    user: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "password",
   });
 
   await pool.query("SELECT NOW()");
